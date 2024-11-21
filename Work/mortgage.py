d4ad905af60e8d77fb12c7a22a214272a32dc423
Aysha Noor <Aysha.Noor@ibm.com>
2024-11-21 15:51:08 +0500

    Exercise 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,23 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+mortgage_period = 0
+mortgage_percent = 0.05
+mortgage_amount = 500000
+monthly_payment = 2684.11
+
+total_paid = 0
+while mortgage_amount > 0:
+
+    if mortgage_period < 12:
+        mortgage_amount = mortgage_amount * (1 + mortgage_percent / 12) - (monthly_payment + 1000)
+        total_paid += monthly_payment + 1000
+    else:
+        mortgage_amount = mortgage_amount * (1 + mortgage_percent / 12) - monthly_payment
+        total_paid += monthly_payment
+
+    mortgage_period += 1
+
+print(round(total_paid, 2), mortgage_period)
+    
