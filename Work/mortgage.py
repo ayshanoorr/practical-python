b584fdd3c1c671ff1834577fd10bdbb9e9f6ca66
Aysha Noor <50948684+ayshanoorr@users.noreply.github.com>
2024-11-25 11:38:08 +0500

    Ex # 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,17 +7,22 @@ mortgage_percent = 0.05
 mortgage_amount = 500000
 monthly_payment = 2684.11
 
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
 total_paid = 0
 while mortgage_amount > 0:
 
-    if mortgage_period < 12:
-        mortgage_amount = mortgage_amount * (1 + mortgage_percent / 12) - (monthly_payment + 1000)
-        total_paid += monthly_payment + 1000
+    if mortgage_period >= extra_payment_start_month and mortgage_period <= extra_payment_end_month:
+        mortgage_amount = mortgage_amount * (1 + mortgage_percent / 12) - (monthly_payment + extra_payment)
+        total_paid += monthly_payment + extra_payment
     else:
         mortgage_amount = mortgage_amount * (1 + mortgage_percent / 12) - monthly_payment
         total_paid += monthly_payment
 
     mortgage_period += 1
+    print(mortgage_period, round(total_paid, 2), round(mortgage_amount, 2))
 
-print(round(total_paid, 2), mortgage_period)
+# print(round(total_paid, 2), mortgage_period)
     
